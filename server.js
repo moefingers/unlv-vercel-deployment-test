da172fa17dc179f91ef980b4c1609e27270dcce0
MoeFingers <mbzuiter@gmail.com>
2024-02-26 19:47:45 -0700

    added some stuff in the response

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ require('dotenv').config()
 const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+    // the following line sends something incredible in response
+    res.send(`<h1>Hello World!</h1>
+    <p>Server is running in port ${PORT}</p>`)
+    
 })
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
